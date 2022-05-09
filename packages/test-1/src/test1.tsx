37694f6934e119af95d03d1eaacedbd4b8fc6796
Michał Czapkowicz <michal.czapkowicz@keen.io>
2022-05-09 15:43:52 +0200

    feat: test

--- packages/test-1/src/test1.tsx
+++ packages/test-1/src/test1.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Test1 = () => (
-  <div>Component Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores assumenda dolor dolorum et explicabo facilis laboriosam officia possimus quo! Error libero natus nihil officiis, omnis veniam vitae! Accusamus, saepe.</div>
+  <div>Component Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores assumenda facilis laboriosam officia possimus quo! Error libero natus nihil officiis, omnis veniam vitae! Accusamus, saepe.</div>
 );
 
 export default Test1;
