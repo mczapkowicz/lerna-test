f274fa95112de9e18e7296aefd4adb3792c98cde
Michał Czapkowicz <michal.czapkowicz@keen.io>
2022-05-12 11:43:39 +0200

    a

--- packages/test-1/src/test1.tsx
+++ packages/test-1/src/test1.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const Test1 = () => (
-  <div>
+const Test1 = () => {
+  const a = "10";
+  const b = (val: number) => {
+    console.log(val + 10)
+  }
+  return (<div>
     Component Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
     asperiores assumenda facilis laboriosam officia possimus quo! Error libero
     natus nihil officiis, omnis veniam vitae! Accusamus, saepe.
-  </div>
-);
+  </div>)
+}
+
 
 export default Test1;
